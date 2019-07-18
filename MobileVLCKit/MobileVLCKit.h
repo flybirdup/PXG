27fd676c70e2d6db48e6a3d215250f38acb7c539
flybirdup <1403348297@qq.com>
2019-07-18 14:31:26 +0800

    Add files via upload

---
+++ MobileVLCKit/MobileVLCKit.h
@@ -0,0 +1,44 @@
+/*****************************************************************************
+ * VLCKit: MobileVLCKit
+ *****************************************************************************
+ * Copyright (C) 2010-2013 Pierre d'Herbemont and VideoLAN
+ *
+ * Authors: Pierre d'Herbemont <pdherbemont # videolan.org>
+ *          Felix Paul Kühne <fkuehne # videolan.org
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+ 
+ //build at 2019.3.14 v2.0.0 for gx
+
+#import "VLCAudio.h"
+#import "VLCLibrary.h"
+#import "VLCMedia.h"
+#import "VLCMediaDiscoverer.h"
+#import "VLCMediaList.h"
+#import "VLCMediaPlayer.h"
+#import "VLCMediaListPlayer.h"
+#import "VLCMediaThumbnailer.h"
+#import "VLCTime.h"
+
+@class VLCMedia;
+@class VLCMediaLibrary;
+@class VLCMediaList;
+@class VLCTime;
+@class VLCVideoView;
+@class VLCAudio;
+@class VLCMediaThumbnailer;
+@class VLCMediaListPlayer;
+@class VLCMediaPlayer;
